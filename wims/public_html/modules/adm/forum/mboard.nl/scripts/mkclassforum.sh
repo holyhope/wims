25b68f1feec56235e7cf88338e7043835c4cf7e9
schaersvoorde <schaersvoorde@b48fb02d-6135-0410-b1dd-deb767c81da9>
2008-06-22 12:38:41 +0000

    [minor/transl] minor language (nl) updates
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@669 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/modules/adm/forum/mboard.nl/scripts/mkclassforum.sh
+++ wims/public_html/modules/adm/forum/mboard.nl/scripts/mkclassforum.sh
@@ -5,7 +5,7 @@ mkdir -p $forumdir/.threads
 
 today=`date '+%Y%m%d'`
 cat <<@ >$forumdir/.def
-!set mb_title=Het bulletin board
+!set mb_title=Het forum
 !set mb_description=Op deze plek kun je discussi&euml;ren met medeleerlingen of docenten.
 !set mb_supervisor=$w_wims_supervisor
 !set mb_email=$w_wims_supervisormail
