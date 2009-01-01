36277cfbc0baa1a9275d9c3aa96b272747dae2cc
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2009-01-01 14:18:18 +0000

    [minor/change] change for swac (continuation)
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@899 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/modules/adm/manage.en/scripts/swac.sh
+++ wims/public_html/modules/adm/manage.en/scripts/swac.sh
@@ -1,23 +1,24 @@
 #! /bin/sh
- packs=$wims_exec_parm
- adresse=packs.shtooka.net
- echo $packs ; 
- 
-cd scripts/data/swac ; 
- for i in $packs; do
-  echo $i ; 
-  wget http://$adresse/$i/$i.mp3.tar; echo "wget $adresse/$i/$i.mp3.tar" ;
-  tar xf $i.mp3.tar;
-  mkdir -p $i;
-  rm -r $i/mp3;
-  mv home/www/packs/$i/mp3 $i/mp3;
-  rm -r home; rm $i.mp3.tar ;
-  cd $i;
-##  mv index.tags.txt index.tags.txt_1 ;
-##  wget http://$adresse/$i/ogg/index.tags.txt ;
-##  echo "wget http://$adresse/$i/ogg/index.tags.txt ";
-##  rm index.tags.txt_1 ;
- cd .. ;\
-done ;
-cd $w_wims_home/scripts/data/swac ; 
-./mkindex ; 
+# too long for the moment
+# do it by hand with $wims/bin/swac
+swac=scripts/data/swac
+cd $swac; 
+./mkindex ;
+!exit
+
+
+# packs=$wims_exec_parm
+#adresse=download.shtooka.net
+# echo $packs ; 
+# swac=scripts/data/swac
+# cd $swac; 
+# for i in $packs; do
+#  j=${i}_mp3 ;
+#  wget http://$adresse/$j.tar; echo "wget $adresse/$j.tar" ;
+#  tar xf $j.tar;
+# if [-d "$j"]  then cd  $j ; cp -r * ../$i/ ; cd $swac; fi ;
+# rm $j.tar ; rm -r $j;
+# echo "$i" >> version ;
+#done ;
+#cd scripts/data/swac ; cp version.tmp version ; rm version.tmp; 
+
