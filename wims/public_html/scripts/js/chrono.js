2b986a8b793589d1427c555338cf548f39cbb09c
reyssat <reyssat@b48fb02d-6135-0410-b1dd-deb767c81da9>
2009-06-29 16:44:59 +0000

    [minor/bug] new chrono avoiding cheating by dragging image to stop it
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@1316 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/scripts/js/chrono.js
+++ wims/public_html/scripts/js/chrono.js
@@ -1,12 +1,17 @@
 
 <script language=javascript type="text/javascript">
-var rtick;
-rtick=$wims_read_parm;
+var fixrtick=$wims_read_parm;
+var rtick=fixrtick;
+var enter=new Date();
+function tempo() {
+	exit=new Date();
+	return Math.ceil((exit.getTime()-enter.getTime())/1000);
+}
 function clock() {
- document.chrono.clock.value=rtick; rtick--;
+ document.chrono.clock.value=rtick; 
+rtick=fixrtick-tempo();
  if(rtick<0) rtick=0;
  Timer= setTimeout("clock()",1000);                    
-}                   
+}                  
 </script>
 !set wims_html_onload=clock();
-
