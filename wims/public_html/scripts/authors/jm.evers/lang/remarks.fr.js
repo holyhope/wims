70d7d8519e92444cb740aec10cc1710096c0cab9
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2010-01-29 20:40:52 +0000

    [minor/language]
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@2378 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/scripts/authors/jm.evers/lang/remarks.fr.js
+++ wims/public_html/scripts/authors/jm.evers/lang/remarks.fr.js
@@ -2,20 +2,20 @@ var r0="Voulez-vous envoyer cette réponse ?\n";
 var r1="Vous n\'avez pas répondu à la question numéro ";
 var r2="Votre réponse n\'est pas réaliste...<br>Je ne la vérifie pas.";
 var r3="J\'ai réécrit la syntaxe sqrt[a](b) pour la racine <br>en une syntaxe b^(1/a)."; 
-var r4="Votre réponse contient une virgule<br>Utiliser la notation avec un point décimal pour les nombres réels...<br> Si vous voulez donner plusieurs réponses, utilisez des mots comme \"and\" \"or\" "
+var r4="Votre réponse contient une virgule<br>Utiliser la notation avec un point décimal pour les nombres réels...<br> Si vous voulez donner plusieurs réponses, utilisez des mots comme \"and\" et \"or\" "
 var r5="Vous devriez remplacer le caractère \"o\" par le nombre \"0\".";
-var r6="Vous essayez de me raconter une histoire.<br> Mais formulez plutôt votre réponse plus sérieusement...";
-var r7="Vous utilisez la lettre  \"x\" comme symbole de multiplication<br>Utilisez plutôt un symbole normal comme \"*\" pour cela...<br>Ou en cas de réponses multiples, écrivez quelque chose comme:<br> x1=123 et x2=456";
+var r6="Vous essayez de me raconter une histoire.<br> Formulez plutôt votre réponse plus sérieusement...";
+var r7="Vous utilisez la lettre  \"x\" comme symbole de multiplication<br>Utilisez plutôt un symbole normal comme \"*\" pour cela ...<br>Ou en cas de réponses multiples, écrivez quelque chose comme:<br> x1=123 et x2=456";
 var r9="Ecrivez une fonction avec des parenthèses. <br>Ainsi, écrivez: sqrt() sin() etc()";
-var r10="Vous n\'avez pas utilisé la bonne notation scientifique...<br> 5*10^03 == 5*10^3 <br> 5*10^2 == 5e+2<br> 5*10^-6 == 5e-6<br>Pas d\'exposants fractionaire...";
-var r11="Une syntaxe spéciale doit être utilisée pour les racines supérieures:<br>sqrt[3](11) = \u221B(17) <br> sqrt[4](23) = \u221C(23)";
-var r12="J\'ai réécrit votre  log[a](b) avec le log en base 10:<br> log(b)/log(a)";
-var r13="Je ne peux pas traiter des crochets pour un autre usage que sqrt[] et log[]...[?]<br>Essayez de réécrire votre réponse ";
+var r10="Vous n\'avez pas utilisé la bonne notation scientifique...<br> 5*10^03 == 5*10^3 <br> 5*10^2 == 5e+2<br> 5*10^-6 == 5e-6<br>Pas d\'exposants fractionaires ...";
+var r11="Une syntaxe spéciale doit être utilisée pour les racines d'ordre supérieure:<br>sqrt[3](11) = \u221B(17) <br> sqrt[4](23) = \u221C(23)";
+var r12="J\'ai réécrit votre log[a](b) avec le log en base 10:<br> log(b)/log(a)";
+var r13="Je ne veux pas traiter les crochets pour un autre usage que sqrt[] et log[]...[?]<br>Essayez de réécrire votre réponse ";
 var r14="Vous devriez réécrire log[10](a) comme log(a).<br>Je vais interpréter log(a) comme un logarithme en base 10.";
 var r15="Vous devriez réécrire log[e](a) comme ln(a).<br>Je vais interpréter ln(a) comme un logarithme en base e.";
 var r16="votre réponse n\'a pas de sens..<br>et n\'est pas acceptée.";
 var r17="c\'est bien sûr :<br> log(a)/log(a) = 1<br> Je ne l\'envoie pas...";
-var r18="Essayez de formuler votre réponse plus précisément :<br>quelquechose comme \"sqrt(5++5)\" ou \"log(5--5)\" peut être écrit beaucoup plus simplement...";
+var r18="Essayez de formuler votre réponse plus précisément :<br>quelque chose comme \"sqrt(5++5)\" ou \"log(5--5)\" peut être écrit beaucoup plus simplement...";
 var r19="Dans votre dessin,vous avez oublié quelques objets fondamentaux.<br>Voir plus bas:<br><br>"; 
 var r20="Votre réponse est illisible et doit être réécrite.<br><br>"; 
 var r21="J\'espérais quelque chose comme:<br> x>123 <br> 12 < x <= 14 <br> Essayez encore...";
@@ -26,9 +26,9 @@ var r25="Question :";
 var r26=" Vous devriez refaire vos calculs...";
 var r27="Dans cet exercice, le pourcentage ne peut être supérieur à 100% ..."
 var r28="Dans cet exercice, le pourcentage ne peut être inférieur à 0% ..."
-var r29="J\'espérais (finalement) quelque chose comme:<br> x = 123 ...ou quelque chose comme ça <br> Essayez encore ";
-var r30="Votre réponse devrait au moins contenir quelques \"flèches\" pour indiquer les pas de votre \"déduction\"<br> Regardez les pages d\'aide pour plus d\'info...";
-var r31="Le nombre minimal de pas [ flèches ->] doit être : ";
+var r29="J\'espérais (finalement) quelque chose comme:<br> x = 123 ...ou quelque chose comme celà <br> Essayez encore ";
+var r30="Votre réponse devrait au moins contenir quelques \"flèches\" pour indiquer les étapes de votre \"déduction\"<br> Regardez les pages d\'aide pour plus d\'info...";
+var r31="Le nombre minimal d\'étapes [ flèches ->] doit être : ";
 var r32="<br>... ce \"texte\" a été retiré de votre réponse.<br> Dans cet exerice, il n\'y en a pas besoin pour déduire la réponse correcte...";
 var r33="Si j\'envoie cette réponse à WIMS<br>elle sera très probablement rejetée... ";
 var r34="Vous avez été pénalisé car votre construction utilisait trop d\'objets."    
