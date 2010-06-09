b48e7fe21ac4379784e0defa725037cb3e62f51f
obado <obado@b48fb02d-6135-0410-b1dd-deb767c81da9>
2010-06-09 15:47:32 +0000

    [major/add] Last version of module adm/raw. Must be tested in several environments to be sure new jobs are working fine.
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@3261 b48fb02d-6135-0410-b1dd-deb767c81da9

---
+++ wims/public_html/modules/adm/raw/scripts/delexo.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+# We use shell script to output oef source file, because
+# otherwise the $ sign will be substituted.
+
+
+logd=$w_wims_home/log/classes/$w_wims_class/src
+oefname=$logd/$wims_exec_parm.oef
+
+if [ ! -f "$oefname" ]; then exit; fi
+
+cat $oefname
+# rm -f : Pas de confirmation d'effacement, pas de diagnostic, pas de code d'erreur lorsqu'un fichier n'existe pas. 
+rm -f $oefname >/dev/null
+rm -fR $oefname $logd/images/$wims_exec_parm >/dev/null
+
+cd modules/classes/$w_lang
+src2def oef
+
