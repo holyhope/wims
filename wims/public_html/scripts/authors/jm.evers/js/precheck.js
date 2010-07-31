d3ed8847b02d8e1bb4cd56f369dc44963794fc2f
schaersvoorde <schaersvoorde@b48fb02d-6135-0410-b1dd-deb767c81da9>
2010-07-31 15:58:21 +0000

    [minor/svn] corrected ? bug in some browsers [typical Opera on WinXP]
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@3365 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/scripts/authors/jm.evers/js/precheck.js
+++ wims/public_html/scripts/authors/jm.evers/js/precheck.js
@@ -36,8 +36,10 @@ function SetTexAppSize(){
 	var p = 1;var size = new Array(2);
 	while ( document.getElementById('TexApp'+p) ){
     	    size = document.getElementById('TexApp'+p).getPrefferedSize();                                                                   
-    	    document.getElementById('TexApp'+p).width = size[0];                                                                             
-    	    document.getElementById('TexApp'+p).height = size[1];
+    	    try{
+		document.getElementById('TexApp'+p).width = size[0];                                                                             
+    		document.getElementById('TexApp'+p).height = size[1];
+	    }catch(e){}// problems on opera/safari
 	    p++;
 	} 
     }                                                 
