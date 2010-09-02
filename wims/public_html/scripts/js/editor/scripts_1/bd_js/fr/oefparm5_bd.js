500955d6b83993ab329733db4feaea052f7b039b
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2010-09-02 19:40:36 +0000

    [minor/typo]
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@3435 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/scripts/js/editor/scripts_1/bd_js/fr/oefparm5_bd.js
+++ wims/public_html/scripts/js/editor/scripts_1/bd_js/fr/oefparm5_bd.js
@@ -1,4 +1,4 @@
-var oefparm5= [ 'reply', 'choice', 'step', 'sc_score', 'help_subject' ];
+var oefparm5= [ 'reply', 'choice', 'step', 'sc_reply', 'help_subject' ];
 function oef5fun(instruction, version){
     switch(instruction)
 	{
@@ -23,8 +23,8 @@ chaine_aide='<code>step</code>'+
 	   break;
 	////******************** 
 
-	case 'sc_score' :
-chaine_aide='<code>sc_score1 sc_score2 ...</code>'+
+	case 'sc_reply' :
+chaine_aide='<code>sc_reply1 sc_reply2 ...</code>'+
 'variable entre 0 et 1 indiquant si la réponse à la question est juste ou non (peut avoir des valeurs entre 0 et 1 pour certains types de réponses).'+
 '' ; return chaine_aide;
 	   break;
