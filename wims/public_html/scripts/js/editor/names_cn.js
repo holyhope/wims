143442a07c0ed5dd20633544aff42e04e5fa234c
zjchen <zjchen@b48fb02d-6135-0410-b1dd-deb767c81da9>
2010-09-22 00:25:35 +0000

    [minor] Chinese translation
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@3472 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/scripts/js/editor/names_cn.js
+++ wims/public_html/scripts/js/editor/names_cn.js
@@ -1,6 +1,44 @@
 var names= new Array();
 
-names["lang_name"]="Chinese";
+names["lang_name"]="中文";
 names["insert_selection"]="插入选择";
-names["not_available"]=names["lang_name"]+" version not yet available";
-names["image_alt"]="image description";
+names["not_available"]=names["lang_name"]+"版尚不可用";
+names["contextual_help"]="与上下文相关的帮助 (自动完成)";
+names["contextual_help_desc"]="输入反斜杠 (<code>\</code>), 然后一个小写字母以激活自动完成功能. 命令表会自动过滤出与你的文本相关的命令.";
+
+
+/** HTML entities ***/
+names["Italics"]="斜体";
+names["Bold"]="粗体";
+
+names["Link"]="链接";
+names["Link_Text"]="链接文本";
+names["Enter_Link_URL"]="输入链接的 URL";
+
+names["Image"]="图像";
+names["image_alt"]="图像描述";
+names["Enter_img_URL"]="输入图像文件名 :";
+
+names["Heading"]="表头 #1";
+names["Unordered_List"]="无序表";
+names["Ordered_List"]="有序表";
+
+/** WIMS 'before' instructions **/
+names["title"]="名称";
+names["User_guide"]="用户指引";
+names["author"]="作者";
+names["range"]="范围";
+names["email"]="电邮";
+
+/** WIMS 'during' instructions  **/ 
+names["statement"]="陈述";
+names["hint"]="提示";
+names["help"]="帮助";
+
+
+/** WIMS 'After' instructions **/
+names["answer"]="回答";
+names["feedback"]="反馈";
+names["condition"]="条件";
+names["solution"]="解答";
+ 
