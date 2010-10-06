9a74a57f4388b1e7f25e4689cce5c9216f16af2a
czzmrn <czzmrn@b48fb02d-6135-0410-b1dd-deb767c81da9>
2010-10-06 19:59:01 +0000

    [minor/transl] fixing italian translation
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@3502 b48fb02d-6135-0410-b1dd-deb767c81da9

---
+++ wims/public_html/scripts/js/editor/names_it.js
@@ -0,0 +1,42 @@
+var names= new Array();
+
+names["lang_name"]="Italiano";
+names["insert_selection"]="inserire la selezione";
+names["not_available"]="La versione "+names["lang_name"]+" non è ancora disponibile.";
+names["contextual_help"]="Help contestuale (con autocompletamento)";
+names["contextual_help_desc"]="Iserisci una <em>backslash</em> (<code>\\</code>) seguita da una lettera minuscola per attivare l'autocompletamento. La lisa dei comandi sarà filtrata di conseguenza.";
+
+/** HTML entities ***/
+names["Italics"]="Corsivo";
+names["Bold"]="Grassetto";
+
+names["Link"]="Link";
+names["Link_Text"]="Testo del link";
+names["Enter_Link_URL"]="Inserire l'URL del link";
+
+names["Image"]="Immagine";
+names["image_alt"]="descrizione dell'immagine";
+names["Enter_img_URL"]="Inserire il nome del file immagine:";
+
+names["Heading"]="Intestazione liv. 1";
+names["Unordered_List"]="Elenco non numerato";
+names["Ordered_List"]="Elenco numerato";
+
+/** WIMS 'before' instructions **/
+names["title"]="Titolo";
+names["User_guide"]="Modo d'uso";
+names["author"]="Autore";
+names["range"]="Intervallo";
+names["email"]="Email";
+
+/** WIMS 'during' instructions  **/ 
+names["statement"]="Enunciato";
+names["hint"]="Suggerimento";
+names["help"]="Help";
+
+
+/** WIMS 'After' instructions **/
+names["answer"]="Risposta";
+names["feedback"]="Feedback";
+names["condition"]="Condizione";
+names["solution"]="Soluzione";
