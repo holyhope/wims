5efbf029224e2e855f01dfd535d3d3f45f88c61c
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2010-12-27 13:29:44 +0000

    [minor/delete] has been transferred in src
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@3661 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/java/Clicktile/make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf ~/.java                                                                                                                             
-rm *.class
-javac -verbose -source 1.4 -target 1.4 *.java
-jar cvf Clicktile.jar *.class
-
-#appletviewer 1.html
-mozilla file://`pwd`/1.html
