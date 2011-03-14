721545ca0f9c3276c877603396a548253d9180b3
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2011-03-14 15:54:43 +0000

    [minor/translation] typo, add wims_name_ , cleanup in make distclean
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@3873 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/src/Flydraw/htmldoc.sh
+++ wims/src/Flydraw/htmldoc.sh
@@ -12,42 +12,41 @@ Each command takes one line.<p>
 In the following table, <tt>[color]</tt>
 may be either a color name, or 3 integers between 0 and 255, separated by commas,
 for the values of red,green,blue.
-<p>\$table_header
-<caption>Drawing commands</caption>
-\$table_hdtr<th colspan=5>Name and syntax
-\$table_hdtr<th colspan=2>Synonymes
-<th colspan=3>Meaning</th></tr><tr><td><td colspan=5>
-"  ; fi 
+"
+drawing="Drawing commands"
+synonym="Synonymes"
+fi ; 
 
 if [ "$lang" = fr ] ; then echo "
 Chaque commande prend une ligne.<p>
 Dans la table suivante <tt>[color]</tt>
 peut 阾re un nom de couleur ou 3 entiers 0 entre 255, s閜ar閟 par des virgules,
-correspondant aux valeurs de  rouge, vert ,bleu.
-<p>\$table_header
-<caption>Commandes</caption>
-\$table_hdtr<th colspan=5>Nom et syntaxe
-\$table_hdtr<th colspan=2>Synonymes
-<th colspan=3>Signification</th></tr><tr><td><td colspan=5>
-" ; fi
+correspondant aux valeurs de  rouge, vert, bleu.
+"
+drawing="Commandes"
+synonym="Synonymes"
+fi ;
 
 if [ "$lang" = cn ] ;  then echo "
 每条命令占一行.<p>
 在下表中, <tt>[色彩]</tt>
 可以是色彩名, 或3个用逗号分隔的0到255的整数, 分别代表红绿蓝三种颜色.
-<p>\$table_header
-<caption>作图命令</caption>
-\$table_hdtr<th colspan=5>名称与语法
-\$table_hdtr<th colspan=2>同义词
-<th colspan=3>含义</th></tr><tr><td><td colspan=5>
-"  ; fi 
-
+"
+drawing="作图命令"
+synonym="同义词"
+fi 
+echo "<p>\$table_header
+ <caption>$drawing</caption>
+ \$table_hdtr<th colspan="5">\$wims_name_name - \$wims_name_Syntax</th>
+ \$table_hdtr<th colspan="2">$synonym</th>
+ <th colspan="3">\$wims_name_Meaning</th></tr>
+"
 awk 'BEGIN {a=10}
 	substr($0,1,1)==":" {a=0}
 	{a++}
-	a==1 {print "$table_tr<td colspan=5 nowrap><tt>"substr($0,2)"</tt></td></tr>"}
-	a==2 {print "$table_tr<td colspan=2><tt>"$0"&nbsp;</tt></td>"}
-	a==3 {print "<td colspan=3>"$0"</td></tr><tr><td colspan=5>"}
+	a==1 {print "$table_tr<td colspan=\"5\" nowrap><tt>"substr($0,2)"</tt></td></tr>"}
+	a==2 {print "$table_tr<td colspan=\"2\"><tt>"$0"&nbsp;</tt></td>"}
+	a==3 {print "<td colspan=\"3\">"$0"</td></tr><tr>"}
 	' $file
 
 echo "\$table_end<p>"
