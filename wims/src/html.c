761b85c2fa696fa4dd683586526f7367d4a00003
obado <obado@b48fb02d-6135-0410-b1dd-deb767c81da9>
2011-12-09 16:20:44 +0000

    [html/css] [Needs Compilation] New template for OEF intro. More compliant with W3C rules for accessibility.
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@4795 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/src/html.c
+++ wims/src/html.c
@@ -39,89 +39,89 @@ void _form_menus(char *p,int kind)
     pp=find_word_end(n);
     if(*pp!=0) *(pp++)=0;
     else {
-	syntax: module_error("syntax_error"); return;
+	 syntax: module_error("syntax_error"); return;
     }
     li=find_word_start(pp); pp=find_word_end(li);
     if(*pp!=0) *(pp++)=0; else goto syntax;
     itemcnt=0;
     if(strcmp(li,"from")==0) {
-	p1=find_word_start(pp);
-	p2=wordchr(p1,"to"); if(p2==NULL || p2<=p1) goto syntax;
-	*(p2-1)=0; p2=find_word_start(p2+strlen("to"));
-	pp=wordchr(p2,"prompt"); if(pp!=NULL) *(pp-1)=0;
-	i1=evalue(p1); i2=evalue(p2);
-	if(i1<-100000000 || i1>100000000 || i2<-100000000 || i2>100000000)
-	  goto syntax;
-	if(i2<i1) return;
-	if(i2>i1+MAX_MENU_ITEMS-1) i2=i1+MAX_MENU_ITEMS-1;
-	itemcnt=i2-i1+1;
-	type=0;
+	 p1=find_word_start(pp);
+	 p2=wordchr(p1,"to"); if(p2==NULL || p2<=p1) goto syntax;
+	 *(p2-1)=0; p2=find_word_start(p2+strlen("to"));
+	 pp=wordchr(p2,"prompt"); if(pp!=NULL) *(pp-1)=0;
+	 i1=evalue(p1); i2=evalue(p2);
+	 if(i1<-100000000 || i1>100000000 || i2<-100000000 || i2>100000000)
+	   goto syntax;
+	 if(i2<i1) return;
+	 if(i2>i1+MAX_MENU_ITEMS-1) i2=i1+MAX_MENU_ITEMS-1;
+	 itemcnt=i2-i1+1;
+	 type=0;
     }
     else {
-	if(strcmp(li,"list")==0) {
+	 if(strcmp(li,"list")==0) {
 	    p1=find_word_start(pp);
 	    pp=wordchr(p1,"prompt"); if(pp!=NULL && pp>p1) *(pp-1)=0;
 	    ovlstrcpy(vbuf,p1);substit(vbuf);
 	    itemcnt=cutitems(vbuf,vlist,MAX_MENU_ITEMS);
 	    type=1;
-	}
-	else goto syntax;
+	 }
+	 else goto syntax;
     }
     for(i=0;i<itemcnt;i++) plist[i]=NULL;
     if(pp!=NULL) {
-	p1=find_word_start(pp+strlen("prompt"));
-	ovlstrcpy(pbuf,p1);substit(pbuf);
-	cutitems(pbuf,plist,MAX_MENU_ITEMS);
+	 p1=find_word_start(pp+strlen("prompt"));
+	 ovlstrcpy(pbuf,p1);substit(pbuf);
+	 cutitems(pbuf,plist,MAX_MENU_ITEMS);
     }
     ovlstrcpy(nbuf,n);substit(nbuf);
     if(kind==FORM_SELECT) {
-	char *pp;
-	pp=getvar("wims_formselect_switch"); if(pp==NULL) pp="";
-	output("<select %s name=\"%s\">\n",pp,nbuf);
+	 char *pp;
+	 pp=getvar("wims_formselect_switch"); if(pp==NULL) pp="";
+	 output("<select %s name=\"%s\" id=\"%s\">\n",pp,nbuf,nbuf);
     }
     if(kind==FORM_BAR) _output_("<b>-</b>");
     val=getvar(nbuf);if(val==NULL) val="";
     for(i=0;i<itemcnt;i++) {
-	if(type==0) {
+	 if(type==0) {
 	    snprintf(buf,sizeof(buf),"%d",i+i1);
 	    p0=buf;
-	}
-	else p0=vlist[i];
-	if(*find_word_start(p0)==0) continue;
-	if(plist[i]==NULL) plist[i]=p0;
-	if(*val!=0 && 
+	 }
+	 else p0=vlist[i];
+	 if(*find_word_start(p0)==0) continue;
+	 if(plist[i]==NULL) plist[i]=p0;
+	 if(*val!=0 && 
 	   (strcmp(p0,val)==0 || 
 	    ( (kind==FORM_SELECT || kind==FORM_CHECKBOX) 
 	     && itemchr(val,p0)!=NULL))) {
 	    if(kind==FORM_SELECT) pc=" selected=\"selected\"";
 	    else pc=" checked=\"checked\"";
-	}
-	else pc="";
-	switch(kind) {
+	 }
+	 else pc="";
+	 switch(kind) {
 	    case FORM_SELECT:
 	    output("<option value=\"%s\"%s>%s</option>\n",p0,pc,plist[i]);
 	    break;
 	    
 	    case FORM_RADIO:
-	    output("<input type=\"radio\" name=\"%s\" value=\"%s\"%s>&nbsp;%s",
-		   nbuf,p0,pc,plist[i]);
+	    output("<input type=\"radio\" name=\"%s\" id=\"%s\" value=\"%s\"%s>&nbsp;%s",
+		   nbuf,nbuf,p0,pc,plist[i]);
 	    if(i<itemcnt-1 && itemcnt>2) _output_(",");
 	    _output_("\n");
 	    break;
 	    
 	    case FORM_CHECKBOX:
-	    output("<input type=\"checkbox\" name=\"%s\" value=\"%s\"%s>&nbsp;%s",
-		   nbuf,p0,pc,plist[i]);
+	    output("<input type=\"checkbox\" name=\"%s\" id=\"%s\" value=\"%s\"%s>&nbsp;%s",
+		   nbuf,nbuf,p0,pc,plist[i]);
 	    if(i<itemcnt-1 && itemcnt>2) _output_(",");
 	    _output_("\n");
 	    break;
 	    
 	    case FORM_BAR:
-	    output("<input type=\"radio\" name=\"%s\" value=\"%s\"%s>",
-		   nbuf,p0,pc);
+	    output("<input type=\"radio\" name=\"%s\" id=\"%s\" value=\"%s\"%s>",
+		   nbuf,nbuf,p0,pc);
 	    break;
 	    
-	}
+	 }
     }
     if(kind==FORM_SELECT) _output_("</select>");
     if(kind==FORM_BAR) _output_("<b>+</b>");
