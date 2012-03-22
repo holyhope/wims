69cf30ea5c6834ee26b6ca008472f8ce69a2c233
schaersvoorde <schaersvoorde@b48fb02d-6135-0410-b1dd-deb767c81da9>
2012-03-22 22:20:43 +0000

    [minor/svn]  Rewrite: should compile on MacOSX ?
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@5177 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/src/Misc/scienceprint/scienceprint.c
+++ wims/src/Misc/scienceprint/scienceprint.c
@@ -1,21 +1,17 @@
 /*
+
 *********************************************************************************
-* J.M. Evers 10/2010                                                            *
-* This is all amateur scriblings... So no copyrights.                           *
-* This source code file, and compiled objects derived from it,                  *
-* can be used and distributed without restriction, including for commercial use *
-* No warrenty whatsoever                                                         *
+* J.M. Evers 3/2012								*
+* This is all amateur scriblings... So no copyrights.				*
+* This source code file, and compiled objects derived from it,			*
+* can be used and distributed without restriction, including for commercial use	*
+* No warrenty whatsoever								*
 *********************************************************************************
-Orignally Written by: http://www.cs.tut.fi/~jkorpela/c/eng.html 7/2000
-
-Corrected usage of 'asprintf()' and modified for wims usage 2/2012 
-
-Used in chemistry and physics to present data in the correct amount of significant digits,
-and in a scientiffic notation.(according the precision of the 'experimental' data)
-
-!exec scienceprint number,significance,type
+WIMS usage:
+sci_num = !exec scienceprint number,significance,type
 number: a number
 desired significance : precision
+
 type (optional args): calc = 0 / html = 1 / latex = 2  / prefix =3 : default 0 or empty
 
 default  : 10-pow notation : 120000,3 -> 120*10^3
@@ -23,18 +19,12 @@ type = 0 : 10-pow notation : 120000,3,0 -> 120*10^3
 type = 1 : html notation   : 120000,3,1 -> 120&times;10<sup>3</sup>
 type = 2 : latex notation  : 120000,3,2 -> 120 \times 10^{3}
 type = 3 : prefix-notation : 120000,3,3 -> 120.0 k
-type = 3 : prefix-notation : 1200000,4,3 -> 1.200 M
 
 multiple conversion: use space between arguments
-!exec scienceprint 120000,4 122900,5 120036,6,3 --> 120.0*10^3,122.90*10^3,120.036 k
+scienceprint 120000,4 122900,5 120036,6,3 --> 120.0*10^3,122.90*10^3,120.036 k
 
-Secure usage of asprintf() : 
--- if ( asprintf() < 0 ) return NULL;
--- free(tmp_memory);
--- check on illegal chars in input
 */
 
-#define _GNU_SOURCE //" needed before stdio.h...warning: implicit declaration of function 'asprintf'"
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
@@ -44,6 +34,72 @@ Secure usage of asprintf() :
 #define PREFIX_END (PREFIX_START+(int)((sizeof(prefix)/sizeof(char *)-1)*3))
 
 
+char *printscience(double value, int sig, int format , int cnt ){
+    static char *prefix[] = {
+	"y", "z", "a", "f", "p", "n", MICRO, "m", "",
+	"k", "M", "G", "T", "P", "E", "Z", "Y"
+    };
+
+    double display, fract;
+    char *sign = NULL;
+    int exponent10;
+
+    if(value < 0.0) {
+	sign = "-";
+	value = -value;
+    } 
+    else 
+    {
+	sign = "";
+    }
+
+    exponent10 = lrint( floor( log10(value) ) );//correctly round to desired precision
+    value *= pow(10.0, sig - 1 - exponent10);
+    fract = modf(value, &display);
+    if(fract >= 0.5){display += 1.0;}
+    value = display * pow(10.0, exponent10 - sig + 1);
+    if(exponent10 > 0){exponent10 = (exponent10/3)*3;}else{exponent10 = ((-exponent10+3)/3)*(-3);}
+    value *= pow(10.0, -exponent10);
+    if (value >= 1000.0) {
+	value /= 1000.0;
+	exponent10 += 3;
+    }
+    else
+    {
+	if(value >= 100.0){
+	    sig -= 2;
+	}
+	else
+	{
+	    if(value >= 10.0){
+		sig -= 1;
+	    }
+	}
+    }
+    if(exponent10 == 0){format = 3;}
+    if(sig < 1){sig = 1;}
+    if(cnt > 1){fprintf(stdout,",");}
+    if(format == 0 || format == 1 || format == 2 || (exponent10 < PREFIX_START) || (exponent10 > PREFIX_END)){
+	if( format == 0){ // 'calculable' presentation
+	    fprintf(stdout, "%s%.*f*10^%d", sign, sig-1, value, exponent10);
+	}
+	else
+	{
+	    if( format == 1 ){ // html presentation
+		fprintf(stdout, "%s%.*f&times;10<sup>%d</sup>", sign, sig-1, value, exponent10);
+	    }
+	    else // latex presentation
+	    {
+		fprintf(stdout, "%s%.*f \\times 10^{%d}", sign, sig-1, value, exponent10);
+	    }
+	}
+    }	    
+    else // format = 3 : prefix presentation
+    {
+	fprintf(stdout, "%s%.*f %s", sign, sig-1, value,prefix[(exponent10-PREFIX_START)/3]);
+    }
+    return NULL;
+}
   
 int main( int argc , char *argv[]){
 
@@ -52,82 +108,10 @@ int main( int argc , char *argv[]){
 	return 0;
     }
     
-    char *result = NULL;
     
-    char *printscience(double value, int sig, int format ){
-	static char *prefix[] = {
-	    "y", "z", "a", "f", "p", "n", MICRO, "m", "",
-	    "k", "M", "G", "T", "P", "E", "Z", "Y"
-	};
-        double display, fract;
-	char *sign = NULL;
-        int exponent10;
-	result = "\0";
-
-	if(value < 0.0) {
-	    sign = "-";
-	    value = -value;
-	} 
-        else 
-	{
-	    sign = "";
-	}
-
-	exponent10 = lrint( floor( log10(value) ) );//correctly round to desired precision
-	value *= pow(10.0, sig - 1 - exponent10);
-	fract = modf(value, &display);
-        if(fract >= 0.5){display += 1.0;}
-	value = display * pow(10.0, exponent10 - sig + 1);
-        if(exponent10 > 0){
-	    exponent10 = (exponent10/3)*3;
-	}
-	else
-	{
-	    exponent10 = ((-exponent10+3)/3)*(-3);
-	}
-	value *= pow(10.0, -exponent10);
-	if (value >= 1000.0) {
-	    value /= 1000.0;
-	    exponent10 += 3;
-	}
-	else
-	{
-	    if(value >= 100.0){
-		sig -= 2;
-	    }
-	    else
-	    {
-		if(value >= 10.0){
-		    sig -= 1;
-		}
-	    }
-	}
-	if(exponent10 == 0){format = 3;}
-	if(sig < 1){sig = 1;}
-	if(format == 0 || format == 1 || format == 2 || (exponent10 < PREFIX_START) || (exponent10 > PREFIX_END)){
-	    if( format == 0){ // 'calculable' presentation
-		if( asprintf(&result, "%s%.*f*10^%d", sign, sig-1, value, exponent10) < 0){ free(result); return NULL;} // secure usage of asprintf()
-	    }
-	    else
-	    {
-		if( format == 1 ){ // html presentation
-		    if( asprintf(&result, "%s%.*f&times;10<sup>%d</sup>", sign, sig-1, value, exponent10) < 0){ free(result);return NULL;} // secure usage of asprintf()
-		}
-	        else // latex presentation
-		{
-		    if( asprintf(&result, "%s%.*f \\times 10^{%d}", sign, sig-1, value, exponent10) < 0){ free(result);return NULL;} // secure usage of asprintf()
-		}
-	    }
-	}	    
-	else // format = 3 : prefix presentation
-	{
-	    if( asprintf(&result, "%s%.*f %s", sign, sig-1, value,prefix[(exponent10-PREFIX_START)/3]) < 0){ free(result);return NULL;} // secure usage of asprintf()
-	}
-	return result;
-    }
 
     double number = 0;
-    int significance = 0,type = 0,first = 0,idx = 0,cnt = 1;
+    int significance = 0,type = 0,idx = 0,cnt = 1;
     char *input = "\0",*ptr = "\0";
 
     /* test for illegal characters */
@@ -161,11 +145,11 @@ int main( int argc , char *argv[]){
 	}
 	// number and precision are mandatory:  default type=0 
 	if( idx < 2 || idx > 3){fprintf(stdout,"syntax error : number1,significance1,type1 number2,significance2,type2 ... number_n,significance_n,type_n \n");return 0;}
-	if(first == 0){ first = 1; fprintf(stdout,"%s", printscience(number, significance, type ) );} else { fprintf(stdout,",%s", printscience(number, significance, type ) );}
+	// call conversion routine
+	printscience(number, significance, type , cnt);
 	cnt++;
 	input = argv[cnt];
     }
     fprintf(stdout,"\n");
-    free(result); // free memory pointer
     return 0;
 }
