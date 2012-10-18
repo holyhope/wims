904c4ccdb5fe183647c9493a73bb2a23b053eb6c
schaersvoorde <schaersvoorde@b48fb02d-6135-0410-b1dd-deb767c81da9>
2012-10-18 10:39:35 +0000

    [minor/svn] add small functionality
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@5801 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/src/Misc/scienceprint/scienceprint.c
+++ wims/src/Misc/scienceprint/scienceprint.c
@@ -8,12 +8,22 @@
 * No warrenty whatsoever							*
 *********************************************************************************
 
+18/10/2012 : 
+Added Mathml output 
+Added option significance=-1 
+To be used when there is no significance known ; just tries to print the number in science notation
+Trying to use the original amount of digits used in "number" argument
+There may be some small flaws in the displayed number of digits (too many) when number is written as "E+123"
+But there never be too few digits (truncation) ; so wims number checking should go fine.
+
+*********************************************************************************
+
 WIMS usage:
 sci_num = !exec scienceprint number,significance,type
 
 number: a number
 significance : desired precision
-type (optional args): calc = 0 / html = 1 / latex = 2  / prefix =3 
+type (optional args): calc = 0 / html = 1 / latex = 2  / prefix = 3  / mathml = 4
 
 default  : calc   notation : 120000,3   -> 1.20*10^5
 type = 0 : calc   notation : 120000,3,0 -> 1.20*10^5
@@ -21,6 +31,7 @@ type = 1 : html notation   : 120000,3,1 -> 1.20&times;10<sup>5</sup>
 type = 2 : latex notation  : 120000,3,2 -> 1.20 \times 10^{5}
 type = 3 : prefix-notation : 120000,3,3 -> 120.0 k
 type = 3 : if -24 > prefix > 24  	-> type = 1 (html)
+type = 4 : mathml notation 
 
 multiple conversion: use space between arguments
 scienceprint 120000,4 122900,5 120036,6,3 --> 120.0*10^3,122.90*10^3,120.036 k
@@ -57,86 +68,113 @@ scienceprint 120000,4 122900,5 120036,6,3 --> 120.0*10^3,122.90*10^3,120.036 k
 #define PREFIX_END 24
 #define MAX_CONV 256
 
-char *printscience(double value, int sig, int format , int cnt ){
-
+char *printscience(double value, int sig, int format , int cnt ,int size){
     static char *prefix[] = { "y", "z", "a", "f", "p", "n", MICRO, "m", "", "k", "M", "G", "T", "P", "E", "Z", "Y" };
     double display, fract;
     char *sign = NULL;
     int exponent10;
-
-    if(value < 0.0) {sign = "-";value = -value;} else {sign = "";}
-
-    exponent10 = lrint( floor( log10(value) ) );//correctly round to desired precision
-    value *= pow(10.0, sig - 1 - exponent10);
-    fract = modf(value, &display);
-    if(fract >= 0.5){display += 1.0;}
-    value = display * pow(10.0, exponent10 - sig + 1);
-    if(exponent10 > 0){exponent10 = (exponent10/3)*3;}else{exponent10 = ((-exponent10+3)/3)*(-3);}
-    value *= pow(10.0, -exponent10);
-    if(format != 3){ // allow all powers; not limited by prefix list
-	if(value > 10.0){
-	    while(value >= 10){
-		value /= 10.0;
-	        exponent10++;
+    if( sig == -1 ){
+     /* no significance truncations...just science notation 1234 -> 1.234*10^3  ; try (!) to use same amount of digits*/
+	sig = size;
+	if(format == 3){format = 1;} /* no prefix --> html noation */
+	if(value < 0.0) {sign = "-";value = -value;sig--;} else {sign = "";}
+	exponent10=0;
+	if(value>=1){
+	    while(value > 10){
+		value=value / 10.0;
+		exponent10++;
+	    }
+	}
+	else /* 0 < value < 1 --> exponent10 < 0 */
+	{
+	    sig = sig - 2; /*  "0." = 2 chars */
+	    while(value < 1){
+		value=value*10;
+		exponent10--;
 	    }
 	}
     }
-    else // steps of powers dividable by 3 used for prefix notation
+    else
     {
-	if (value >= 1000.0) {
-	    value /= 1000.0;
-	    exponent10 += 3;
+	if(value < 0.0) {sign = "-";value = -value;} else {sign = "";}
+	exponent10 = lrint( floor( log10(value) ) );/* correctly round to desired precision */
+	value *= pow(10.0, sig - 1 - exponent10);
+	fract = modf(value, &display);
+	if(fract >= 0.5){display += 1.0;}
+	value = display * pow(10.0, exponent10 - sig + 1);
+	if(exponent10 > 0){exponent10 = (exponent10/3)*3;}else{exponent10 = ((-exponent10+3)/3)*(-3);}
+	value *= pow(10.0, -exponent10);
+	if(format != 3){ /* allow all powers; not limited by prefix list */
+	    if(value > 10.0){
+		while(value >= 10){
+		    value = value / 10.0;
+	    	    exponent10++;
+		}
+	    }
 	}
-	else
+	else /* steps of powers dividable by 3 used for prefix notation */
 	{
-	    if(value >= 100.0){
-		sig -= 2;
+	    if (value >= 1000.0) {
+		value = value / 1000.0;
+		exponent10 = exponent10 + 3;
 	    }
 	    else
 	    {
-		if(value >= 10.0){
-		    sig -= 1;
+		if(value >= 100.0){
+		    sig =  sig - 2;
+		}
+		else
+		{
+		    if(value >= 10.0){
+			sig = sig - 1;
+		    }
 		}
 	    }
 	}
     }
     if(cnt > 1){fprintf(stdout,",");}
-    // check on format style versus exponent
+    /* check on format style versus exponent */
     if(exponent10 == 0 ){
-	format = 3; // do not use 1.23*10^0
+	format = 3; /* do not use 1.23*10^0 */
     }
     else
     {
 	if(exponent10 == 1){
-	    format = 3;// do not use 1.23*10^1
+	    format = 3;/* do not use 1.23*10^1 */
 	    value = value * 10;
 	    sig = sig - 1;
 	}
 	else
 	{
 	    if(format == 3 && ((exponent10 < PREFIX_START) || (exponent10 > PREFIX_END))){ 
-		format = 1; // if no prefix available, revert to html presentation
+		format = 1; /* if no prefix available, revert to html presentation */
 	    }
 	}
     
     }
     if(sig < 1){sig = 1;}
     if(format != 3 ){
-	if( format == 0){ // 'calculable' presentation
+	if( format == 0){ /* 'calculable' presentation */
 	    fprintf(stdout, "%s%.*f*10^%d", sign, sig-1, value, exponent10);
 	}
 	else
 	{
-	    if( format == 1 ){ // html presentation
+	    if( format == 1 ){ /* html presentation */
 		fprintf(stdout, "%s%.*f&times;10<sup>%d</sup>", sign, sig-1, value, exponent10);
 	    }
-	    else // latex presentation
+	    else 
 	    {
-		fprintf(stdout, "%s%.*f \\times 10^{%d}", sign, sig-1, value, exponent10);
+		if(format == 2 ){/* latex presentation */
+		    fprintf(stdout, "%s%.*f \\times 10^{%d}", sign, sig-1, value, exponent10);
+		}
+		else
+		{ /* mathml presentation */
+		    fprintf(stdout,"<math xmlns=\"http://www.w3.org/1998/Math/MathML\" display=\"inline\"><mstyle id=\"wims_mathml\" mathsize=\"110%%\"><mn>%s%.*f</mn><mo>&times;</mo><msup><mn>10</mn><mn>%d</mn></msup></mstyle></math>", sign, sig-1, value, exponent10);
+		}
 	    }
 	}
-    }	    
-    else // format = 3 : prefix presentation or other more suitable presentation
+    }
+    else /* format = 3 : prefix presentation or other more suitable presentation */
     {
 	fprintf(stdout, "%s%.*f %s", sign, sig-1, value,prefix[(exponent10-PREFIX_START)/3]);
     }
@@ -149,9 +187,9 @@ int main( int argc , char *argv[]){
 	fprintf(stdout,"syntax error : number1,significance1,type1 number2,significance2,type2 ... number_n,significance_n,type_n \n");
 	return 0;
     }
-    
+
     double number = 0;
-    int significance = 0,type = 0,idx = 0,cnt = 1;
+    int significance = 0,type = 0,idx = 0,cnt = 1,size = 0;
     char *input = "\0",*ptr = "\0";
 
     /* test for illegal characters */
@@ -162,32 +200,33 @@ int main( int argc , char *argv[]){
     input = argv[cnt];
     while( input != NULL ){
 	if(cnt > MAX_CONV){fprintf(stdout,"\nerror: number of conversions exceeds limit of %d\n",MAX_CONV);return 0;}
-        while (*input){ // loop through invalid chars.
+        while (*input){ /* loop through invalid chars. */
 	    if ( strchr(invalid_characters, *input) ){
-		fprintf(stdout,"\nerror : found illegal characterin argument \"%s\" \n",input);
+		fprintf(stdout,"\nerror : illegal character \"%s\" \n",input);
 		return 0;
 	    }
 	    input++;
 	}
-	// reset input to actual value
+	/* reset input to actual value */
 	input = argv[cnt];
 	ptr = (char *) strtok(input,",");
 	idx = 0;
 	type = 0;
+	size = 0;
 	while( ptr != NULL ){
 	    switch( idx ){
-		case 0: number = atof(ptr); break;
-		case 1: significance = atoi(ptr); break;
-		case 2: type = atoi(ptr); if(type != 0 && type != 1 && type != 2 && type != 3){type = 0;} break;
+		case 0: number = atof(ptr); size = strlen(ptr);if( strstr(ptr,".") != NULL){ size = size - 1 ;}; break;
+		case 1: significance = atoi(ptr);  break;
+		case 2: type = atoi(ptr); if(type < 0 || type > 4 ){type = 0;} break;
 		default: break;
 	    }
 	    idx++;
 	    ptr = (char *) strtok(NULL,",");
 	}
-	// number and precision are mandatory:  default type=0 
+	/* number and precision are mandatory:  default type=0  */
 	if( idx < 2 || idx > 3){fprintf(stdout,"\nsyntax error : number1,significance1,type1 number2,significance2,type2 ... number_n,significance_n,type_n \n");return 0;}
-	// call conversion routine
-	printscience(number, significance, type , cnt);
+	/* call conversion routine */
+	printscience(number, significance, type , cnt , size);
 	cnt++;
 	input = argv[cnt];
     }
