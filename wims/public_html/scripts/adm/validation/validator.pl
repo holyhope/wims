1dda9e97a5981592b5f581d8ef9c6c665c167fe5
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2013-02-04 16:04:18 +0000

    [major/work in progress] add possibility to see the html warnings and errors in modtool. for the moment, must be authorized by the administrator (interface not done, for test add the line validator_address=http:xxxx/w3c-validator/check in log/wims.conf)
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@6348 b48fb02d-6135-0410-b1dd-deb767c81da9

---
+++ wims/public_html/scripts/adm/validation/validator.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+use WebService::Validator::HTML::W3C;
+
+## validator file validator_uri
+my $file = shift(@ARGV);
+my $uri = shift(@ARGV);
+ my $v = WebService::Validator::HTML::W3C->new(detailed => 1);
+ $v->validator_uri($uri);
+ $uri=$v->validator_uri();
+ ##printf("Validation by %s\n",$uri);
+ if ( $v->validate_file("$file") ) {
+   if ( !$v->is_valid )
+   {
+     foreach my $E (@{$v->errors}) {
+       ##next if ($E->msg =~ /not allowed in prolog/);
+       next if ($E->msg =~ /Conflict between Mime Type/); #error coming from last.html
+       next if ($E->msg =~ /xmlParseEntityRef/); #error I do not understand, link with &
+       printf("\$wims_name_Error: line %d, %s\n", $E->line, $E->msg);
+       
+     }
+     foreach my $E (@{$v->warnings}) {
+       next if ($E->msg =~ /character "(&amp;|&)" is the first character of a delimiter but occurred as data/);  ##due to wims
+       next if ($E->msg =~ /Conflict between Mime Type/);  #error coming from last.html
+       next if ($E->msg =~ /xmlParseEntityRef/); #error I do not understand, link with &
+       printf("\$wims_name_warning: line %d, %s\n", $E->line, $E->msg);}
+   }
+ } else {
+     printf ("Failed to validate the website $v->uri : %s\n", $v->validator_error);
+ }
