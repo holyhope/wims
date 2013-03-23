5baf1492922983961049cecdd5560649224e006a
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2013-03-23 18:46:29 +0000

    [minor/] script modindclass.pl makes almost the same job than modind.c for example classes
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@6464 b48fb02d-6135-0410-b1dd-deb767c81da9

---
+++ wims/public_html/bases/sys/modindclass.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl
+##example classes
+
+use warnings;
+use strict;
+
+my $dir='../../../log/classes/';
+## list of all domains in domain/domain
+my %ref= ();
+my $ref=\%ref;
+my @list=listclass();
+my $dirout="../class";
+$ref=refclass(@list);
+my @Key = ('supervisor','domain','level','lang','institution','description',"addr",'keywords','title');
+my @Lang= ('fr','en','it','nl') ; 
+my %T = ('version' => 'version',
+'lang' => 'language',
+'institution' => 'title',
+'supervisor' => 'author',
+'description' => 'description',
+'level' => 'level',
+);
+my %keywords=();
+
+for my $key (@Key) {
+  my $text='';
+  for my $cl (@list) {
+    $text .=  "$cl:"; 
+    $text .= $ref{$cl}{$key} if ($ref{$cl}{$key});
+    $text .= "\n";
+  };
+  out("$dirout/$T{$key}",join("\n",sort( split("\n", $text)))) if ($T{$key}) ; 
+}
+for my $l (@Lang) { my $text='';
+  for my $cl (@list) { 
+    next if (!($ref{$cl}{'lang'} =~ /$l/)) ;
+    for my $k (split(',',$ref{$cl}{'keywords'})) {
+      if ($keywords{$k}) { $keywords{$k} .= " " . $cl  } else { $keywords{$k} = $cl};
+    }
+    for my $k (split(',',$ref{$cl}{'domain'})) {
+      if ($keywords{$k}) { $keywords{$k} .= " " . $cl  } else { $keywords{$k} = $cl};
+    }
+ }
+   while ( my ($key, $value) = each (%keywords) ) {
+     $text .= "$key:$value\n" ; 
+   }
+   out("$dirout/$l", $text); 
+}
+
+sub refclass { my @L=@_ ; 
+  for my $cl (@L) {
+    open IN, "$dir/$cl/.def";
+    while(<IN>) {
+    if ($_ =~ /!set class_(\w+)=(.*)/) { $ref{$cl}{$1}=$2 ;}}
+    close IN;
+  }
+}
+
+sub listclass { 
+ my @L= () ;
+ for my $cl (glob("$dir/*")) {
+   $cl=~s /$dir\/// ; 
+   push @L, $cl if (!($cl =~ /[A-Z]+/) && $cl < 10000) ;
+  }
+  @L
+}
+sub out { my ($bloc, $text) = @_;
+  open  (OUT, ">$bloc") ;
+  print OUT $text ; close OUT;
+}
+
+sub sortuniq {
+  my $prev = "not $_[0]";
+  grep { $_ ne $prev && ($prev = $_, 1) } sort @_;
+}
+
+#for my $tag (keys %{$hash{text}}) {}
