ef30ad07d313b240a2eec29c682f2de8c7da91b4
obado <obado@b48fb02d-6135-0410-b1dd-deb767c81da9>
2013-03-27 09:01:38 +0000

    [js/jquery] Adds ".multicomplete" class wich allows autocomplete multiple keywords on one field. use ".autocomplete" class if you want a single keyword. replace example on domains in modtool properties
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@6486 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/scripts/js/interface/autocomplete.js
+++ wims/public_html/scripts/js/interface/autocomplete.js
@@ -5,8 +5,35 @@ jQuery(function($) {
   /* Autocompletion des domaines dans Modtool 
       attention il faut remplir la variable "tags" pour afficher des mots clés
   */
-     
-     $(".autocomplete").autocomplete({
+  
+    function comma_split( val ) {
+      return val.split( /,\s*/ );
+    }
+    function extractLast( term ) {
+      return comma_split( term ).pop();
+    }
+
+
+
+    $(".multicomplete").autocomplete({
+        source: function( request, response ) {
+          // delegate back to autocomplete, but extract the last term
+          response( $.ui.autocomplete.filter(tags, extractLast( request.term ) ) );
+        },
+        select: function( event, ui ) {
+          var terms = comma_split( this.value );
+          // remove the current input
+          terms.pop();
+          // add the selected item
+          terms.push( ui.item.value );
+          // add placeholder to get the comma-and-space at the end
+          terms.push( "" );
+          this.value = terms.join( ", " );
+          return false;
+        }
+    });
+
+    $(".autocomplete").autocomplete({
       source: function( request, response ) {
               
               var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
