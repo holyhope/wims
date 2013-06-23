2bbca941e34c7918a109536f77df21d4c0759458
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2013-06-23 19:56:23 +0000

    [minor/revert] revert patch 6777 : GNU specific, does not work  in POSIX.
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@6783 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/modules/adm/manage/scripts/attrib2.sh
+++ wims/public_html/modules/adm/manage/scripts/attrib2.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 cd ..
-test=`find $wims_exec_parm -perm /22 \! -type l 2>/dev/null | head -1000`
-test2=`find $w_nowrite2 -maxdepth 0 -perm /22 \! -type l 2>/dev/null | head -1000`
+test=`find $wims_exec_parm -perm +22 \! -type l 2>/dev/null | head -1000`
+test2=`find $w_nowrite2 -maxdepth 0 -perm +22 \! -type l 2>/dev/null | head -1000`
 echo "$test
 $test2" | head -60
 if [ ! -z "$test$test2" ]; then chmod og-w $test $test2; fi
