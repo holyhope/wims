516e859a4a7e1c4d62145e876787ed79c8a84e1d
reyssat <reyssat@b48fb02d-6135-0410-b1dd-deb767c81da9>
2013-07-11 20:24:32 +0000

    [minor/documentation] add comments in mkdomain.pl
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@6821 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/bases/sys/mkdomain.pl
+++ wims/public_html/bases/sys/mkdomain.pl
@@ -1,19 +1,36 @@
 #!/usr/bin/perl
 
+# From the distribution files 
+# domain : structured list of all (sub)domains (words containing underscores)
+# domain.xx : list of correspondance between domain names and their translation (spaces allowed) in language xx.
+# this script constructs the following files
+# reversedomain  : list of parents of each domain
+# domain.json    : list of domains up to third level
+# domain.xx.json : sorted list of translations of domain names (without domain_name itself)
+# domain.xx.tmp  : should be (not checked) domains of file domain for which a translation exists in domain.xx.
+##                 In practice it is (not checked) same as domain.xx, 
+##                 with empty translation replaced by left member (= domain_name) of record
+##                 and completed with domains appearing in domain but not in domain.xx. 
+##                 This is also sorted.
+
 use warnings;
 use strict;
 use search ('hashdomain', 'listdomain', 'out', 'sortuniq', 'treate_domainfile');
 
 $/ = undef;
 my $dir='domain';
-## list of all domains in domain/domain
 
+## Make sorted list of all domains in domain/domain
 my @list=listdomain("$dir/domain");
+
+## For translators : make a domain.template as a base for domain.xx file 
+## Helpful for creation of new language or to check for completeness of current domain.xx file
 #my $text=join(":\n", @list) . ":\n"; 
 #$text=~ s/^://g;
 #$text=~ s/ +\n/\n/g;
 #out("$dir/domain.template", $text);
 
+## Put in domain/reversedomain the list of parents of each domain.
 my %ref= hashdomain("$dir/domain");
 my $ref=\%ref;
 my $TEXT="##generated\n";
@@ -23,9 +40,12 @@ for my $tag (keys %ref) {
 
 out("$dir/reversedomain",$TEXT);
 
-##domain list in the three  first levels
+## Put in domain/domain.json the sorted list of domains up to third level
 out("$dir/domain.json", domainjson(%ref));
 
+## for languages for which domain.xx exists (why not "de" ?), construct files
+## domain.xx.tmp : sorted list of domains names and their translation, approximately union of domain and domain.xx
+## domain.xx.json : sorted list of translations of domain names (without domain_name itself) 
 for my $la ('fr','en','it','si','cn','nl','ca','es') {
  next if !(-e "$dir/domain.$la");
  my %dom = treate_domainfile ("$dir/domain.$la");
