7249e63221522ad2364b9995d265555606d4ebfa
obado <obado@b48fb02d-6135-0410-b1dd-deb767c81da9>
2013-09-01 21:06:53 +0000

    [js] new way to do autocomplete. can switch to another keywords database without reloading page.
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@6939 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/scripts/js/interface/autocomplete.js
+++ wims/public_html/scripts/js/interface/autocomplete.js
@@ -1,50 +1,53 @@
-jQuery.noConflict();
 
-jQuery(function($) {
-    
-  /* Autocompletion d'un champ texte 
-      attention il faut remplir la variable javascript "tags" pour afficher des mots clés
-      ajoutez la classe "multicomplete" ou "autocomplete" au choix sur l'input, selon que vous 
-      souhaitez autoriser un ou plusieurs mot-clés.
-  */
-  
-    function comma_split( val ) {
-      return val.split( /,\s*/ );
-    }
-    function extractLast( term ) {
-      return comma_split( term ).pop();
-    }
+/********* Autocomplete.js  ************************************************************************
+    Autocompletion d'un champ texte 
+    ajoutez la classe "multicomplete" ou "autocomplete" au choix sur l'input, selon que vous 
+    souhaitez autoriser un ou plusieurs mot-clés.
+    Puis lancez set_multicomplete, ou set_autocomplete avec la liste des mots clés en parametre
+****************************************************************************************************/
 
-    $(".multicomplete")
-      // don't navigate away from the field on tab when selecting an item
-      .bind( "keydown", function( event ) {
-        if (event.keyCode === $.ui.keyCode.TAB &&  $( this ).data( "ui-autocomplete" ).menu.active ) {
-          event.preventDefault();
-        }
-      })
-      //Adds Autocompletion
-      .autocomplete({
-        focus: function() {
-          // prevent value inserted on focus
-          return false;
-        },
-        source: function( request, response ) {
-          // delegate back to autocomplete, but extract the last term
-          response( $.ui.autocomplete.filter(tags, extractLast( request.term ) ) );
-        },
-        select: function( event, ui ) {
-          var terms = comma_split( this.value );
-          // remove the current input
-          terms.pop();
-          // add the selected item
-          terms.push( ui.item.value );
-          // add placeholder to get the comma-and-space at the end
-          terms.push("");
-          this.value = terms.join(", ");
-          return false;
-        }
+
+function comma_split( val ) {
+  return val.split( /,\s*/ );
+}
+
+function extractLast( term ) {
+  return comma_split( term ).pop();
+}
+
+function set_multicomplete(tags){
+  $(".multicomplete")
+    // don't navigate away from the field on tab when selecting an item
+    .bind( "keydown", function( event ) {
+      if (event.keyCode === $.ui.keyCode.TAB &&  $( this ).data( "ui-autocomplete" ).menu.active ) {
+        event.preventDefault();
+      }
+    })
+    //Adds Autocompletion
+    .autocomplete({
+      focus: function() {
+        // prevent value inserted on focus
+        return false;
+      },
+      source: function( request, response ) {
+        // delegate back to autocomplete, but extract the last term
+        response( $.ui.autocomplete.filter(tags, extractLast( request.term ) ) );
+      },
+      select: function( event, ui ) {
+        var terms = comma_split( this.value );
+        // remove the current input
+        terms.pop();
+        // add the selected item
+        terms.push( ui.item.value );
+        // add placeholder to get the comma-and-space at the end
+        terms.push("");
+        this.value = terms.join(", ");
+        return false;
+      }
     });
+}
 
+function set_autocomplete(tags){
     $(".autocomplete").autocomplete({
         source: function( request, response ) {
             var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
@@ -53,4 +56,4 @@ jQuery(function($) {
             }) );
         }
     });
-});
+}
