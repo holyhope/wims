4fff9f6d2f374d8b8e3cbc2cee7b5b53e78642b8
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2013-11-04 10:50:15 +0000

    [minor/formatage]
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@7159 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/modules/adm/class/activity/stat.pl
+++ wims/public_html/modules/adm/class/activity/stat.pl
@@ -1,68 +1,68 @@
 #!/usr/bin/perl
-#use strict ;
+#use strict;
 
 use Time::Local;
-my ($OUT, $SHEET) ; 
-push (@ARGV,split(' ', $ENV{'wims_exec_parm'})) if ($ENV{'wims_exec_parm'}) ; 
+my ($OUT, $SHEET);
+push (@ARGV,split(' ', $ENV{'wims_exec_parm'})) if ($ENV{'wims_exec_parm'});
 while ($_ = shift (@ARGV))
 {
   last if (!/^--/);
      if (/^--file=(.*)$/) { $OUT   = $1; } # fichier de sortie
      if (/^--sheet=(.*)$/) {$SHEET = $1; } # nombre de feuilles
-} ;
+};
 #fichier à lire
 my $FILE = "/" . $_;
-my (%lastdate, %score, %duree,$dattime) ;  
+my (%lastdate, %score, %duree,$dattime);
 for my $sh (1..$SHEET) {
-   $lastdate{$sh}='' ; 
-   $score{$sh} = 0 ; 
+   $lastdate{$sh}='';
+   $score{$sh} = 0;
 }
 
 my $session=' ';
-my $nbsessions = 0 ; 
+my $nbsessions = 0;
   #le fichier est récupéré ordonné par dates croissantes
-open(IN, $FILE) ;
-while(<IN>){ 
+open(IN, $FILE);
+while(<IN>){
  #extraction de l'info de la ligne
  #20070914.23:52:03 6BA720BDC3  1  1 score 10  	82.238.218.220
  #next si ligne d'examen
  next if (/^E/);
- @_ = split(/ +/) ;
- $dattime=$_[0] ; 
+ @_ = split(/ +/);
+ $dattime=$_[0];
  if ($_[4] eq 'score' || (!$lastdate{$_[2]}) ) {
-  if (!($session eq $_[1])){ $nbsessions ++ ; $session = $_[1] ; } ;
-  $dattime=~/([0-9]+)\.([0-9:]+)/ ; 
+  if (!($session eq $_[1])){ $nbsessions ++; $session = $_[1]; };
+  $dattime=~/([0-9]+)\.([0-9:]+)/;
   my ($date,$time) = ($1,$2);
   $date =~/([0-9][0-9][0-9][0-9])([0-9][0-9])([0-9][0-9])/;
-  my ($annee,$mois,$jour)=($1,$2,$3) ;
+  my ($annee,$mois,$jour)=($1,$2,$3);
   $date=converttime($time,$date);
   if(!$lastdate{$_[2]} && $_[4] eq 'new'){#on initialise s'il n'avait jamais fait cette feuille :
-	   $lastdate{$_[2]}=$date;
-	   $duree{$_[2]}= 0;
-	   next ; 
+      $lastdate{$_[2]}=$date;
+      $duree{$_[2]}= 0;
+      next;
    } else {
 #on juge que s'il y a moins de 15mn entre deux dates consecutives, le bonhomme a travaille pendant ce temps
-	    if ($date-$lastdate{$_[2]}<900){
-	       $duree{$_[2]} +=  ($date-$lastdate{$_[2]}); 
-	    } else { }
-	      $lastdate{$_[2]}=$date;
+        if ($date-$lastdate{$_[2]}<900){
+           $duree{$_[2]} +=  ($date-$lastdate{$_[2]});
+        } else { }
+          $lastdate{$_[2]}=$date;
   }
-  if ($_[4] eq 'score') {; 
-    $score{$_[2]} ++ ;
-   ## $feuille{$_[2]} = 1 ; $exo{$_[2] . '.' . $_[3]} ++ ;#ne me sert à rien
-  } ;  ##nombre de fois pour l'exo feuille.numero 
- } ;
+  if ($_[4] eq 'score') {;
+    $score{$_[2]} ++;
+   ## $feuille{$_[2]} = 1; $exo{$_[2] . '.' . $_[3]} ++;#ne me sert à rien
+  };  ##nombre de fois pour l'exo feuille.numero
+ };
 }
 
-my ($text,$score_global,$duree_globale,$nombresessions_globale)= (' ',0,0,0) ; 
+my ($text,$score_global,$duree_globale,$nombresessions_globale)= (' ',0,0,0);
 for  my $sh (1..$SHEET) {
- $text .= $score{$sh} . ',' . converttime2($duree{$sh}) . ',' ;
- $score_global += $score{$sh} ; $duree_globale += $duree{$sh} ; 
+ $text .= $score{$sh} . ',' . converttime2($duree{$sh}) . ',';
+ $score_global += $score{$sh}; $duree_globale += $duree{$sh};
 }
 #dernière connexion,nb_sessions,nb exos abordés total, nb exos par feuille, temps par feuille
-print $dattime . ',' . $nbsessions 
-      . ',' . $score_global . ',' . converttime2($duree_globale) 
-      . "," . $text ;
+print $dattime . ',' . $nbsessions
+      . ',' . $score_global . ',' . converttime2($duree_globale)
+      . "," . $text;
 
 sub converttime {
     my $heure=shift;
@@ -74,7 +74,6 @@ sub converttime {
     my $mois=$2;
     my $jour=$3;
     return timelocal(0,0,0,$jour,$mois - 1,$annee) + $heure;
-    
 }
 
 sub converttime2 {
