ccb4860afb0ceef8cced4b41adfbf6891ff75401
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2014-08-29 09:22:40 +0000

    [minor/] put directly yacasheader in yacas.c
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@8137 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/src/Interfaces/yacas.c
+++ wims/src/Interfaces/yacas.c
@@ -35,10 +35,90 @@ char *outprompt="Out>";
 
 char *nameofcmd="yacas -p";
 
-#include "yacasheader.c"
+char *firstupper[]={
+"abs",
+"and",
+"arg",
+"complex",
+"conjugate",
+"cos",
+"cosh",
+"cot",
+"coth",
+"csc",
+"csch",
+"denom",
+"echo",
+"exp",
+"factor",
+"factorize",
+"for",
+"i",
+"if",
+"im",
+"infinity",
+"integrate",
+"limit",
+"ln",
+"max",
+"min",
+"not",
+"or",
+"pi",
+"print",
+"random",
+"re",
+"round",
+"sec",
+"sech",
+"sign",
+"simplify",
+"sin",
+"sinh",
+"solve",
+"sqrt",
+"tan",
+"tanh",
+"undefined",
+"until",
+"while",
+};
+int firstupperno=(sizeof(firstupper)/sizeof(firstupper[0]));
+
+char header[]="\
+tg(x) := Tan(x)\n\
+log(x) := Ln(x)\n\
+e := Exp(1)\n\
+E := Exp(1)\n\
+sgn(x) := Sign(x)\n\
+ch(x) := Cosh(x)\n\
+sh(x) := Sinh(x)\n\
+th(x) := Tanh(x)\n\
+acos(x) := ArcCos(x)\n\
+arccos(x) := ArcCos(x)\n\
+asin(x) := ArcSin(x)\n\
+arcsin(x) := ArcSin(x)\n\
+atan(x) := ArcTan(x)\n\
+arctan(x) := ArcTan(x)\n\
+arctg(x) := ArcTan(x)\n\
+Argch(x) := ArcCosh(x)\n\
+argch(x) := ArcCosh(x)\n\
+acosh(x) := ArcCosh(x)\n\
+Argsh(x) := ArcSinh(x)\n\
+argsh(x) := ArcSinh(x)\n\
+asinh(x) := ArcSinh(x)\n\
+Argth(x) := ArcTanh(x)\n\
+argth(x) := ArcTanh(x)\n\
+atanh(x) := ArcTanh(x)\n\
+ctg(x) := Cot(x)\n\
+rint(x) := Round(x)\n\
+RANDOM() := Random()\n\
+SOLVE(x,y) := Solve(x,y)\n\
+";
+
 
 struct {
-    char *wname;    char *defaultval;    char *yacasset;
+    char *wname; char *defaultval; char *yacasset;
 } setups[]={
       {"w_yacas_precision", "20", "\\precision=",},
       {"w_yacas_serieslength", "8", "\\serieslength="}
