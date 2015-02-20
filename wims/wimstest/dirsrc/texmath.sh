04a4c85c1ab3162396b1cbd1b40e82cfe9cfa7d6
reyssat <reyssat@b48fb02d-6135-0410-b1dd-deb767c81da9>
2015-02-20 06:56:41 +0000

    [test] adding texmath tests
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@8570 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/wimstest/dirsrc/texmath.sh
+++ wims/wimstest/dirsrc/texmath.sh
@@ -3,4 +3,9 @@
 wims_home=`pwd`;
 wims_dirtest="$wims_home/wimstest/dirtest";
 
-$wims_home/src/wims test $wims_dirtest texmath 't1 t2 t3'
+$wims_home/src/wims test $wims_dirtest texmath 'add2 add2p add3'
+$wims_home/src/wims test $wims_dirtest texmath 'sign doublesign'
+$wims_home/src/wims test $wims_dirtest texmath 'mul div muldiv muldivp'
+$wims_home/src/wims test $wims_dirtest texmath 'exp ind num'
+$wims_home/src/wims test $wims_dirtest texmath 'mixed1 mixed2 mixed3 mixed4 mixed5'
+$wims_home/src/wims test $wims_dirtest texmath 'stupid warn1ok warn1'
