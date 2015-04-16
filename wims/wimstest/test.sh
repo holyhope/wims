da91dabd748e248712a087dd9506cf9e6e6cad17
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2015-04-16 06:43:56 +0000

    [test] add result directory in test - change the script to compare. work in progress
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@8799 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/wimstest/test.sh
+++ wims/wimstest/test.sh
@@ -4,6 +4,7 @@
 wims_home=`pwd`;
 wims_dirsrc="$wims_home/wimstest/dirsrc";
 wims_tmp="$wims_home/wimstest/tmp";
+wims_res="$wims_home/wimstest/dirres";
 mkdir -p $wims_home/wimstest/tmp
 
 if [ "$1" ] ; then
@@ -14,7 +15,13 @@ if [ "$1" ] ; then
   done;
 else
   for i in `ls $wims_dirsrc/*.sh`; do
-   j=`basename $i .sh`; echo $j
-   $i > $wims_tmp/$j
+   j=`basename $i .sh`; echo "Testing $j...\c"
+   $i > $wims_tmp/$j 2>&1
+   if ! cmp $wims_res/$j $wims_tmp/$j; then
+     diff -c $wims_res/$j $wims_tmp/$j > $wims_tmp/$j.diff;
+     echo "BUG"
+   else
+     echo "OK"
+   fi
   done
 fi ;
