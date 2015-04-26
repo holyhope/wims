45fe96cbeec4f8c794c62e498b16115c4326cce1
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2015-04-26 09:14:09 +0000

    [test] 	add while
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@8822 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/wimstest/dirsrc/if.sh
+++ wims/wimstest/dirsrc/if.sh
@@ -3,7 +3,8 @@
 wims_home=`pwd`;
 wims_dirtest="$wims_home/wimstest/dirtest";
 
-test="test1 test2 test3 test4 test5"
+test="test1 test2 test3 test4 test5
+test6 test7"
 
 for a in $test; do
  $wims_home/src/wims test $wims_dirtest if $a
