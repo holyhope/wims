e501fe22ecedfda3f70ae0342a0318adbd3c8e01
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2015-05-05 15:25:09 +0000

    [help] - add some edit_area id for special methods - improve the help - add debug_mode for special methods with multiple answers - no more default displays in inputfields (too confusing).
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@8867 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/themes/mkcss.pl
+++ wims/public_html/themes/mkcss.pl
@@ -15,7 +15,10 @@ my $char="---";
 
 for my $f (glob("*/*.css.template")) {
   my $f1= $f ; $f1 =~ s/.template// ; my $f2 = "$f1.tmp.css" ;
+  my @f3=split("/",$f1);
+  my $f3=$f3[0];
   my $TEXT ="/*! generated from $f - do not modify directly */\n" ;
+  my %colors;
   open(IN,"$f");
   while (<IN> ) {
    my $line=$_ ;
@@ -25,11 +28,27 @@ for my $f (glob("*/*.css.template")) {
  }
  close IN ;
  $TEXT .= "\n";
+ # lecture de oefcolors
+ my $fc="$f3/oefcolors";
+ if (!(-e $fc)) { $fc="oefcolors" };
+ open(INN, "$fc");
+ $/ = "\n";
+ while(<INN>) {
+  if( /!set (\w+)=(\w+)/) { $colors{$1}=$2;}
+ }
+ close INN;
+ ## remplacement des couleurs oef
+ my $colors=\%colors;
+ for my $k (keys \%colors) {
+   $TEXT =~ s/color: *$k/color:$colors{$k}/g;
+ }
+ 
  out("$f2" ,$TEXT) ;
+ $/ = undef;
  system("java -jar yuicompressor $f2 -o $f1 -v; rm $f2");
 }
 
-
+sub t { my ($c1,$c2,$L)=@_; $L->{$c1}=$c2};
 sub treate { my ($c)= @_ ;
   return if ((!$c) || ($c =~ /#/));
   open(INN, $c);
