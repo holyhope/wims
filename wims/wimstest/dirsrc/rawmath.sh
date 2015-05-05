abd4d2c1ee28ea719a970d1c576b0ad1f2e0ae2f
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2015-05-05 13:14:27 +0000

    [test]
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@8865 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/wimstest/dirsrc/rawmath.sh
+++ wims/wimstest/dirsrc/rawmath.sh
@@ -11,7 +11,8 @@ export force_mathml=
 
 
 test="t1 t2 t3 t4
-t5 t6 t7"
+t5 t6 t7 t8 t9
+t10 t11 t12 t13 t14 t15"
 
 for a in $test; do
  $wims_home/src/wims test $wims_dirtest rawmath $a
--- wims/wimstest/test.sh
+++ wims/wimstest/test.sh
@@ -5,8 +5,10 @@ wims_home=`pwd`;
 wims_dirsrc="$wims_home/wimstest/dirsrc";
 wims_tmp="$wims_home/wimstest/tmp";
 wims_res="$wims_home/wimstest/dirres";
-mkdir -p $wims_home/wimstest/tmp
+wims_dirtest="$wims_home/wimstest/dirtest";
 
+mkdir -p $wims_tmp
+mkdir -p $wims_tmp/diroef
 if [ "$1" ] ; then
   for a in $(seq $#) ; do opt=$1;
     $wims_dirsrc/$opt.sh > $wims_tmp/$opt;
@@ -17,11 +19,24 @@ else
   for i in `ls $wims_dirsrc/*.sh`; do
    j=`basename $i .sh`; echo "Testing $j... \c"
    $i > $wims_tmp/$j 2>&1
-   if ! cmp $wims_tmp/$j $wims_res/$j; then
-     diff -c $wims_res/$j $wims_tmp/$j > $wims_tmp/$j.diff;
-     echo "BUG"
+   if [ "$j" = "oef" ] ; then 
+     for ii in `ls $wims_dirtest/diroef/*` ; do
+      jj=`basename $ii`; echo "Testing $jj... \c"
+       if ! cmp $wims_tmp/diroef/$jj $wims_res/diroef/$jj; then
+         diff -c $wims_res/diroef/$jj $wims_tmp/diroef/$jj > $wims_tmp/diroef/$jj.diff;
+         echo "CHANGE"
+       else
+         echo "OK"
+       fi
+     done
    else
-     echo "OK"
+   echo $i
+     if ! cmp $wims_tmp/$j $wims_res/$j; then
+      diff -c $wims_res/$j $wims_tmp/$j > $wims_tmp/$j.diff;
+      echo "CHANGE"
+     else
+      echo "OK"
+     fi
    fi
   done
 fi ;
