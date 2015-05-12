ef4985a9df3e035b39eabfc64491a0f28ba61f78
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2015-05-12 16:11:51 +0000

    [test]
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@8930 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/wimstest/dirsrc/translator.sh
+++ wims/wimstest/dirsrc/translator.sh
@@ -7,8 +7,6 @@ export w_dictionary=$wims_dirtest/dic.sorted
 export w_suffix_dictionary=
 export wims_exec_parm="titi tutu xxxx"
 $wims_home/src/Misc/translator
-echo "\n"
-
-echo "leave:\n"
 export w_translator_unknown=leave
+
 $wims_home/src/Misc/translator
