990e76c95d40d19809926eb38b8a7cefcaa62daa
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2015-05-12 15:54:26 +0000

    [test]
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@8927 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/wimstest/dirsrc/random.sh
+++ wims/wimstest/dirsrc/random.sh
@@ -4,7 +4,8 @@ wims_home=`pwd`;
 wims_dirtest="$wims_home/wimstest/dirtest";
 
 test="test1 test2 test3
-test4 test5 test6"
+test4 test5 test6
+test7 test8 test9"
 
 for a in $test; do
  $wims_home/src/wims test $wims_dirtest random $a
