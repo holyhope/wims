2dfa6516edc04e5e71d7afa934f3eb5288919f85
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2015-05-21 17:46:13 +0000

    [test]
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@8950 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/wimstest/dirsrc/pari.sh
+++ wims/wimstest/dirsrc/pari.sh
@@ -10,7 +10,8 @@ export pari_command="gp -f -q -p 150000"
 
 export wims_exec_parm="2^17-1"
 $wims_home/src/Interfaces/pari
-export wims_exec_parm="random(500)"
-$wims_home/src/Interfaces/pari
-export wims_exec_parm="parirandom(10000)"
-$wims_home/src/Interfaces/pari
+#there is a seed in pari.c so change each time
+#export wims_exec_parm="random\(500)"
+#$wims_home/src/Interfaces/pari
+#export wims_exec_parm="parirandom(10000)"
+#$wims_home/src/Interfaces/pari
