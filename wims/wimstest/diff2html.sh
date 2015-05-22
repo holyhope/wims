670f34af808337434415634d8b1004878d4fdd4c
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2015-05-22 08:25:05 +0000

    [test for oef - work in progress] use encrypt - must have create the diff for OEF exercices
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@8954 b48fb02d-6135-0410-b1dd-deb767c81da9

---
+++ wims/wimstest/diff2html.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+IND="ALLOEFHTML"
+mkdir -p $IND
+ echo '<!DOCTYPE html PUBLIC "-//IETF//DTD HTML 2.0//EN"><HTML><HEAD><TITLE>Enscript Output</TITLE></HEAD><BODY><ul>' > index.html
+>> index.html
+for j in `ls ALLOEF/*` ; do
+   jj=`basename $j`
+   enscript --color -w html -2 --header="<a href=index.html>retour</a>" -Ediffu $j -o ALLOEFHTML/$jj.html ;
+   echo "<li><a href="$IND/$jj.html">$jj</a>" >> index.html
+done
+
+echo "</ul></body></html>" >> index.html
