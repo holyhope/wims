86106a61168db6b9844170385ef7385b681ec6cb
guerimand <guerimand@b48fb02d-6135-0410-b1dd-deb767c81da9>
2016-06-22 09:33:46 +0000

    [minor] comment in score.c
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@10364 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/src/score.c
+++ wims/src/score.c
@@ -352,11 +352,11 @@ int _subword(char bf2[],char *ftbuf)
     else
       mkfname(fbuf,"%s/%s/.users/%s",class_base,classp2,userp);
     getdef(fbuf,buf,buf2);
-    if(buf2[0]==0) ovlstrcpy(buf2,"EMPTY");
+    if(buf2[0]==0) ovlstrcpy(buf2,"EMPTY"); /* in case of no value define for the user*/
     /* get time restriction for this value */
     snprintf(buf,sizeof(buf),"techvar_%s",buf2);
     /*mkfname(fbuf,"%s/%s/.E%s",class_base,classp,sheet);*/
-    getdef(ftbuf,buf,buf2);
+    getdef(ftbuf,buf,buf2);  /* read time restriction correspond to the value of technival variable in the file of the sheet or exam*/
     if(buf2[0]==0) ovlstrcpy(buf2,"none");
     /*string_modify(bf2,p1,p2,buf2);*/
     bf2=buf2;
@@ -398,6 +398,7 @@ int _getscorestatus(char *classe, int sheet)
   if(isexam || score_isexam) es="E"; else es="";
   accessfile(nbuf,"r","%s/%s/.%s%d",class_base,classe,es,sheet);
   if(*find_word_start(nbuf)==0) return 1;
+/* preparing score restriction file name :/*
   mkfname(ftbuf,"%s/%s/.%s%d",class_base,classe,es,sheet);
   return _subword(nbuf,ftbuf);
 }
