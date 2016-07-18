ffe77ced26bf6cc4d4a3a4fce6c48edb64bd7970
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2016-07-18 09:28:48 +0000

    [minor] update validator (waiting to have an html5 header)
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@10817 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/public_html/scripts/adm/validation/validator.pl
+++ wims/public_html/scripts/adm/validation/validator.pl
@@ -14,8 +14,12 @@
        next if ($E->msg =~ /not allowed in prolog/);
        next if ($E->msg =~ /Conflict between Mime Type/); #error coming from last.html
        next if ($E->msg =~ /xmlParseEntityRef/); #error I do not understand, link with &
+       next if ($E->msg =~ /data-dropdown-menu/); #is not a member of a group specified for any attribute (html5)
+       next if ($E->msg =~ /no attribute \"data/); #html5
+       next if ($E->msg =~ /aria-label/);  #html5
+       next if ($E->msg =~ /nav/);  #html5
        printf("\$wims_name_Error: line %d, %s\n", $E->line, $E->msg);
-       
+
      }
      foreach my $E (@{$v->warnings}) {
        next if ($E->msg =~ /character "(&amp;|&)" is the first character of a delimiter but occurred as data/);  ##due to wims
@@ -23,7 +27,7 @@
        next if ($E->msg =~ /xmlParseEntityRef/); #error I do not understand, link with &
        printf("\$wims_name_warning: line %d, %s\n", $E->line, $E->msg);}
    } else {
-     printf("Validation has been done") ; }
+     printf("Validation has been done"); }
  } else {
      printf ("Failed to validate the validator $v->uri : %s\n", $v->validator_error);
  }
