cd85e5524ed8ed9da287179c9376819b025a28e3
bpr <bpr@b48fb02d-6135-0410-b1dd-deb767c81da9>
2017-05-08 17:24:01 +0000

    [wimstest] validation. Add an option for more cleaning of the files.   problem yet with audio and svg   all zones if there is not a second argument
    git-svn-id: https://subversion.renater.fr/wimsdev/trunk@11502 b48fb02d-6135-0410-b1dd-deb767c81da9

--- wims/wimstest/validation/validator_clean.pl
+++ wims/wimstest/validation/validator_clean.pl
@@ -11,15 +11,17 @@ my $warning1='is the first character of a delimiter but occurred as data';
 my $warning2="info: line \$";
 my $warning21=": character \"&\" is the first character of a delimiter but occurred as data";
 my $DIR=".";
+my $option='';
 while ($_ = shift(@ARGV))
 {
   last if (/^$/);
-  if  (/^(.*)$/)   {$DIR=$1 }
+  if (/^--option=(.*)$/)  { $option = $1}
+  elsif  (/^--dir=(.*)$/)   {$DIR = $1 }
 }
-
 my @Files = GetFilesList ($DIR);
 foreach my $File  (@Files) {
 	treate($File);
+	if($option==1){ treate2($File)};
 }
 
 sub treate { my ($file)=@_;
@@ -28,16 +30,37 @@ sub treate { my ($file)=@_;
   while (<IN>) {
    next if (/$warning1/);
    next if (/$warning2\d+$warning21/);
-   next if (/data-dropdown-menu/); #is not a member of a group specified for any attribute (html5)
+   next if (/data-dropdown/); #is not a member of a group specified for any attribute (html5)
    next if (/no attribute \"data/); #html5
    next if (/aria-label/);  #html5
    next if (/nav/);  #html5
+   next if (/no attribute \"autocomplete/);
+   next if (/validating: test.html/);
    $text .= $_;
   }
   close IN;
   out($file, $text);
 }
 
+sub treate2 { my ($file)=@_;
+  my $text='';
+  open(IN,$file);
+  while (<IN>) {
+   next if (/start tag was here/);
+   next if (/value of attribute "type" cannot be "search"/);
+   next if (/there is no attribute "results"/);
+   next if (/there is no attribute "placeholder"/);
+   next if (/there is no attribute "name"/);
+   next if (/there is no attribute "type"/);
+   next if (/document type does not allow element "style" here/);
+   next if (/document type does not allow element "div" here\s*END/); ## last in the result come from the debug
+   $text .= $_;
+  }
+  close IN;
+  $text =~ s/line \d+://g;
+  $text =~ s/END//g;
+  out($file, $text);
+}
 
 sub out { my ($bloc, $text) = @_;
   open  (OUT, ">$bloc") || warn "peut pas créer $bloc";
